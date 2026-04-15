19f4b864ae62d26bde11641656dc7f546caa4818
Eduardo Correia <webdantas@gmail.com>
2026-04-15 16:56:07 -0300

    feat(frontend): initial structure with api layer and folders organization

---
+++ frontend/src/api/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000/api',
+});
+
+// Interceptor para token (já deixa pronto pro login)
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+export default api;
